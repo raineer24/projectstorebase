1ef8060f708a4c15cd0e42c177b4a7639923bd70
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-11-23 10:41:54 +0800

    Add new components register-facebook and register-form

---
+++ src/app/auth/components/register/register-facebook/register-facebook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-facebook',
+  templateUrl: './register-facebook.component.html',
+  styleUrls: ['./register-facebook.component.scss']
+})
+export class RegisterFacebookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
