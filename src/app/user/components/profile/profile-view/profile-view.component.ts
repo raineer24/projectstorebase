67fe270f7d3e51f41f113ec10c68437d49a00835
Mark Jusef Julio <dzulai@gmail.com>
2017-12-03 16:13:45 +0800

    Implement profile view component and UI

--- src/app/user/components/profile/profile-view/profile-view.component.ts
+++ src/app/user/components/profile/profile-view/profile-view.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../interfaces';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.scss']
 })
-export class ProfileViewComponent implements OnInit {
+export class ProfileViewComponent implements OnInit, OnDestroy {
+  @Output() onUpdateClickEmit: EventEmitter<string> = new EventEmitter();
+  profileViewSubs: Subscription;
+  userData: {
+    'email': string,
+    'lastName': string,
+    'firstName': string,
+    'gender': string,
+    'mobileNumber': string
+  };
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.userData = JSON.parse(localStorage.getItem('user'));
+  }
+
+  onUpdateClick() {
+    this.onUpdateClickEmit.emit();
+  }
+
+  ngOnDestroy() {
+    if (this.profileViewSubs) { this.profileViewSubs.unsubscribe() }
   }
 
 }
--- src/app/user/components/profile/profile.component.ts
+++ src/app/user/components/profile/profile.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  isEditMode: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.isEditMode = false;
+  }
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
   }
 
 }
