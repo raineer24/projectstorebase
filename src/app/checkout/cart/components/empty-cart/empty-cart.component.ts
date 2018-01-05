1a0656dc62fd06d632164027f242d4cb05818012
Raineer <delaritaraineer81@gmail.com>
2018-01-05 14:16:30 +0800

    View car items from database

--- src/app/checkout/cart/components/empty-cart/empty-cart.component.ts
+++ src/app/checkout/cart/components/empty-cart/empty-cart.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from "@angular/core";
+import { ProductActions } from "./../../../../product/actions/product-actions";
+import { Item } from "./../../../../core/models/item";
+import { AppState } from "./../../../../interfaces";
+import { Store } from "@ngrx/store";
+import { getProducts, getTaxonomies } from "./../../../../product/reducers/selectors";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'app-empty-cart',
-  templateUrl: './empty-cart.component.html',
-  styleUrls: ['./empty-cart.component.scss']
+  selector: "app-empty-cart",
+  templateUrl: "./empty-cart.component.html",
+  styleUrls: ["./empty-cart.component.scss"]
 })
-export class EmptyCartComponent implements OnInit {
+export class EmptyCartComponent implements OnInit, OnDestroy {
+  products$: Observable<any>;
+  @Input() item: Item;
+  @Input() items;
+  @Input() product: Item;
+  constructor(
+    private productActions: ProductActions,
+    private store: Store<AppState>
+  ) {}
 
-  constructor() { }
+  // Items = [
+  //   {
+  //      name: 'Mango',
+  //      description: 'Organic fruits'
+  //   }
 
-  Items = [
-    {
-       namedescription: 'Mango',
-       description: 'Organic fruits'
-    }
-      
-  ];
+  // ];
 
   ngOnInit() {
+    this.products$ = this.store.select(getProducts);
   }
 
+  ngOnDestroy() {}
 }
