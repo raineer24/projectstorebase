5fd92183a343e14c2eda66c9fc9f710b9445455b
Raineer <delaritaraineer81@gmail.com>
2018-01-09 15:22:52 +0800

    ENV PORT 8081

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
+ 
 export const environment = {
   production: false,
-  // API_ENDPOINT: 'http://localhost:8081/',
+  API_ENDPOINT: 'http://localhost:8081/',
   // API_ENDPOINT: 'https://angularspree-new.herokuapp.com/',
-  API_ENDPOINT: 'http://grocer-api-dev.ap-southeast-2.elasticbeanstalk.com/',
+  //API_ENDPOINT: 'http://grocer-api-dev.ap-southeast-2.elasticbeanstalk.com/',
   AppName: 'OMG!',
   FACEBOOK_APP_ID: '1858414594186264',
   FACEBOOK_SRC: '//connect.facebook.net/en_US/sdk.js'
