8a146ea7f33dbafef52e292b98ef38c91d6fe6c4
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2018-01-13 05:37:59 +0800

    Add rxjs/add/operator/map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { SpinnerModule } from "primeng/primeng";
 
 
 // adding rx operators
-// import "rxjs/add/operator/map";
+import "rxjs/add/operator/map";
 import "rxjs/add/operator/filter";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/catch";
