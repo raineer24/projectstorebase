def0ea797563b8befa85f138a9b79d05d34fe516
Victor Michael Queja <victor.queja@gmail.com>
2018-01-25 21:08:16 +0800

    Align center megamenu and restore home-route-ts to original state

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,7 +1,6 @@
 import { HomeComponent } from './home.component';
-import { ItemListEntryComponent } from './content/item-list/item-list-entry/item-list-entry.component';
 
 export const HomeRoutes = [
   { path: '', component: HomeComponent },
-  { path: 'item/:code/:string', component: ItemListEntryComponent }
+  { path: 'item/:id/:string', component: HomeComponent }
 ];
