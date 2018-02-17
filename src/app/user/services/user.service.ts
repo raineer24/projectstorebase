88dd8bd20232fa7e05b69bf6cb9dc266ef344f7e
Mark Jusef Julio <mjjulio@Marks-MacBook-Pro.local>
2018-02-17 18:04:53 +0800

    Create user list API calls

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -58,4 +58,105 @@ export class UserService {
       .catch(res => Observable.empty());
   }
 
+  /**
+   *
+   *
+   * @returns {Observable<any>}
+   *
+   * @memberof UserService
+   */
+  getLists(): Observable<any> {
+    const userId = 1;
+    return this.http.get(`v1/list/${userId}/user`)
+      .map((res: Response) => res.json())
+      .catch(res => Observable.empty());
+  }
+
+  /**
+   *
+   *
+   * @returns {Observable<any>}
+   *
+   * @memberof UserService
+   */
+  createNewList(params): Observable<any> {
+    const userId = 1;
+    return this.http.post(`v1/${userId}/user`,{
+      userId: userId,
+      name: params.name
+    })
+      .map((res: Response) => res.json())
+      .catch(res => Observable.empty());
+  }
+
+  /**
+   *
+   *
+   * @returns {Observable<any>}
+   *
+   * @memberof UserService
+   */
+  updateList(params: any): Observable<any> {
+    const userId = 1;
+    return this.http.put(`v1/list/${params.id}`,{
+      name: params.name,
+      description: params.description
+    }).map((res: Response) => res.json()
+    ).catch(res => Observable.empty());
+  }
+
+  /**
+   *
+   *
+   * @returns {Observable<any>}
+   *
+   * @memberof UserService
+   */
+  deleteList(id: number): Observable<any> {
+    return this.http.delete(`v1/list/${id}`
+    ).map((res: Response) => res.json()
+    ).catch(res => Observable.empty());
+  }
+
+
+  /**
+   *
+   *
+   * @returns {Observable<any>}
+   *
+   * @memberof UserService
+   */
+  getListItems(listId: number): Observable<any> {
+    return this.http.get(`v1/listitems/${listId}/list`)
+      .map((res: Response) => res.json())
+      .catch(res => Observable.empty());
+  }
+
+  addListItem(params: any): Observable<any> {
+    const userId = 1;
+    return this.http.post(`v1/listitems`,{
+
+      })
+      .map((res: Response) => res.json())
+      .catch(res => Observable.empty());
+  }
+
+  //removeListItem
+  removeListItem(id: number): Observable<any> {
+    const userId = 1;
+    return this.http.delete(`v1/listitems/${id}`)
+      .map((res: Response) => res.json())
+      .catch(res => Observable.empty());
+  }
+
+/*
+GET list/{useraccount_id} = return all user's list
+POST list {useraccount_id} = create new list
+PUT list/{id} {payload} = edit list name
+DELETE list/{id}= delete list
+GET listitem/{listitem_id} = return all items in the list
+PUT listitem/{listitem_id} {list_id, item_id, user_id} = add item to list
+DELETE listitem/{listitem_id} = delete item from list
+*/
+
 }
