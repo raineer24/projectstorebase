21beeac0a31928e6e7dff7f6e642a89e98d2dc37
Raineer <delaritaraineer81@gmail.com>
2018-04-07 14:08:07 +0800

    listmap is not a function

--- src/app/user/components/reset-pass/reset-pass.component.ts
+++ src/app/user/components/reset-pass/reset-pass.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
 @Component({
   selector: 'app-reset-pass',
   templateUrl: './reset-pass.component.html',
   styleUrls: ['./reset-pass.component.scss']
 })
 export class ResetPassComponent implements OnInit {
-
-  constructor() { }
+  resetForm: FormGroup
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    const password = '';
+    const verify = '';
+
+    this.resetForm = this.fb.group({
+      'password': [password, Validators.compose([Validators.required, Validators.minLength(6)])],
+      'verify': [verify, Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
   }
+
+ 
 
 }
