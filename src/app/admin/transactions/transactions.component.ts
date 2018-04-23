ee677aadd1c4d97f1e6f26f1116ec757e34c0d48
Raineer <delaritaraineer81@gmail.com>
2018-04-23 17:19:48 +0800

    View list

--- src/app/admin/transactions/transactions.component.ts
+++ src/app/admin/transactions/transactions.component.ts
@@ -104,15 +104,6 @@ export class TransactionsComponent implements OnInit {
     return this.transaction.filter(obj => obj.checked);
   }
 }
-getCustomers(criteria: CustomerSearchCriteria): Transaction[] {
-  return this.transaction.sort((a, b) => {
-    if (criteria.sortDirection === 'desc') {
-      return a[criteria.sortColumn] < b[criteria.sortColumn];
-    }
-    else {
-      return b[criteria.sortColumn] > a[criteria.sortColumn];
-    }
-  }); 
 
 
 export class Transaction {
