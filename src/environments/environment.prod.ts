57f3ac65085d8124996043d3bbc2818ee5503eaf
Victor Michael Queja <victor.queja@gmail.com>
2018-07-14 22:42:18 +0800

    new prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'http://grocer-api-dev.ap-southeast-2.elasticbeanstalk.com/',
+  // API_ENDPOINT: 'http://grocer-api-dev.ap-southeast-2.elasticbeanstalk.com/',
+  API_ENDPOINT: 'http://grocer-api-prod.ap-southeast-2.elasticbeanstalk.com/',
   AppName: 'OMG!',
   FACEBOOK_APP_ID: '1858414594186264',
   FACEBOOK_SRC: '//connect.facebook.net/en_US/sdk.js',
